ec491c9850dcf2f45cd8e99272c76571d31e1cfd
Andy <amillion3@gmail.com>
2018-02-26 20:30:38 -0600

    exercise 2 complete

--- strings.js
+++ strings.js
@@ -1,6 +1,10 @@
 console.log("Hello");
 
 //Challenge exercise 1
-
 var numOfSandwiches = 2 * 4;
 console.log("numOfSandwiches: ", numOfSandwiches);
+
+//Challenge exercise 2
+var name = "Andy";
+var string = "Hello " + name + ", how are you?";
+console.log("string: ", string);
